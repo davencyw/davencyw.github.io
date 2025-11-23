5a58c256ccc3044e6179d110bf2d46c644bc2531
david schmidig <david@schmidig.net>
2025-11-23 17:18:35 +0100

    feat: relative path

--- page/vite.config.js
+++ page/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 
 
   export default defineConfig({
-    base: '/davencyw.net/', // Set this to match your repository name
+    base: '/', // Set to root for custom domain
     assetsInclude: ['**/projects/**/*.html', '**/projects/**/*.png'], // Include all necessary files under projects/
   })
